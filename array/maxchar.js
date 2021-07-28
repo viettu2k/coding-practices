a5f38ce3874caf341330ed6b19ce848e7d6cc736
viettu2k <doanviettu2k@gmail.com>
2021-07-28 11:01:55 +0700

    array coding practices

---
+++ array/maxchar.js
@@ -0,0 +1,35 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+function maxChar(str) {
+    const charMap = {};
+    let max = 0;
+    let maxChar = "";
+
+    for (let char of str) {
+        // common
+        // if (charMap[char]) {
+        //   charMap[char]++
+        // } else {
+        //   charMap[char] = 1;
+        // }
+
+        // optimal
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+
+    for (let char in charMap) {
+        if (charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char;
+        }
+    }
+
+    return maxChar;
+}
+
+console.log(maxChar("holomoly"));
---
+++ array/palindrome.js
@@ -0,0 +1,23 @@
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
+// solution 1
+function palindrome(str) {
+    let rev = str.split("").reverse().join("");
+    return rev === str;
+}
+
+// solution 2
+// function palindrome(str) {
+//     return str.split("").every((char, index) => {
+//         return char === str[str.length - 1 - index];
+//     });
+// }
+
+console.log(palindrome("abccba"));

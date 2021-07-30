5c472cef28ca03be755f038764b9ea563ed520f5
viettu2k <doanviettu2k@gmail.com>
2021-07-30 19:34:42 +0700

    implement queue and add weave , change location

---
+++ queue/implementQueue.js
@@ -0,0 +1,37 @@
+// --- Description
+// Create a queue data structure.  The queue
+// should be a class with methods 'add' and 'remove'.
+// Adding to the queue should store an element until
+// it is removed
+// --- Examples
+//     const q = new Queue();
+//     q.add(1);
+//     q.remove(); // returns 1;
+
+class Queue {
+    constructor() {
+        this.data = [];
+    }
+
+    add(record) {
+        this.data.unshift(record);
+    }
+
+    remove() {
+        return this.data.pop();
+    }
+
+    peek() {
+        return this.data[this.data.length - 1];
+    }
+}
+
+const q = new Queue();
+q.add(1);
+q.add(2);
+q.add(3);
+q.add(4);
+q.add(5);
+console.log(q);
+
+module.exports = Queue;

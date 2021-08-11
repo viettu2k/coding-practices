1b48d4f617edc02a5b0a2960ee969ae094cd0338
viettu2k <doanviettu2k@gmail.com>
2021-08-11 09:47:28 +0700

    binary gap

---
+++ array/binaryGap.js
@@ -0,0 +1,34 @@
+// Directions
+/* A binary gap within a positive integer N is any maximal sequence of consecutive
+ zeros that is surrounded by ones at both ends in the binary representation of N.
+For example, number 9 has binary representation 1001 and contains a binary gap of length 2. 
+The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 
+and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. 
+The number 15 has binary representation 1111 and has no binary gaps. The number 32 has binary representation 100000 
+and has no binary gaps. */
+
+// solution 1
+// Time complexity: O(n)
+// Space complexity: O(n)
+function binaryGap(number) {
+    let binary = "";
+    while (number > 0) {
+        binary = (number % 2) + binary;
+        number = Math.floor(number / 2);
+    }
+
+    let max = 0;
+    let counter = 0;
+    for (let element of binary) {
+        if (element === "1") {
+            max = Math.max(max, counter);
+            counter = 0;
+        } else {
+            counter++;
+        }
+    }
+
+    return max;
+}
+
+console.log(binaryGap(529));

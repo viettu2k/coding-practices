3cd26e6e9f48263faa04887b3c1336f217ce48d6
viettu2k <doanviettu2k@gmail.com>
2021-08-13 07:35:51 +0700

    contains common item fix time complexity

--- array/containsCommonItem.js
+++ array/containsCommonItem.js
@@ -3,12 +3,14 @@
 (true/fasle) whether these two arrays contain any common items */
 
 // solution 1
-// Time complexity: O(n*m)
+// Time complexity: O(a1 * a2)
 // Space complexity: O(1)
 function containsCommonItems(arr1, arr2) {
-    for (let i = 0; i < arr1.length; i++) {
-        for (let j = 0; j < arr2.length; j++) {
-            if (arr1[i] === arr2[j]) return true;
+    for (let element1 of arr1) {
+        for (let element2 of arr2) {
+            if (element1 === element2) {
+                return true;
+            }
         }
     }
 
@@ -16,21 +18,26 @@ function containsCommonItems(arr1, arr2) {
 }
 
 // solution 2
-// Time complexity: O(n) || O(m)
-// Space complexity: O(n)
+// Time complexity: O(a1 + a2)
+// Space complexity: O(a1)
 function containsCommonItems(arr1, arr2) {
     let visited = {};
-    for (let i = 0; i < arr1.length; i++) {
-        visited[arr1[i]] = true;
+    for (let element1 of arr1) {
+        visited[element1] = true;
     }
 
-    for (let i = 0; i < arr2.length; i++) {
-        if (visited[arr2[i]]) {
+    for (let element2 of arr2) {
+        if (visited[element2]) {
             return true;
         }
     }
 
     return false;
+}
+
+// solution 3
+function containsCommonItems(arr1, arr2) {
+    return arr1.some((item) => arr2.includes(item));
 }
 
 console.log(containsCommonItems(["a", "b", "c", "x"], ["z", "y", "x"]));

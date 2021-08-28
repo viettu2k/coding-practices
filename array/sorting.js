016db8635733d554f5465149c7040527019141d6
viettu2k <doanviettu2k@gmail.com>
2021-08-28 15:48:52 +0700

    add quickSort and  insertionSort

--- array/sorting.js
+++ array/sorting.js
@@ -1,6 +1,9 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
+// Implement insertionSort, quickSort
 
+// Time complexity: O(n^2)
+// Space complexity: O(1)
 function bubbleSort(arr) {
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length - i - 1; j++) {
@@ -13,6 +16,8 @@ function bubbleSort(arr) {
     return arr;
 }
 
+// Time complexity: O(n^2)
+// Space complexity: O(1)
 function selectionSort(arr) {
     for (let i = 0; i < arr.length; i++) {
         let indexOfMin = i;
@@ -31,6 +36,26 @@ function selectionSort(arr) {
     return arr;
 }
 
+// Time complexity: O(n^2)
+// Space complexity: O(1)
+function insertionSort(arr) {
+    let length = arr.length;
+    for (let i = 1; i < length; i++) {
+        // Choosing the first element in our unsorted subarray
+        let current = arr[i];
+        // The last element of our sorted subarray
+        let j = i - 1;
+        while (j > -1 && current < arr[j]) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = current;
+    }
+    return arr;
+}
+
+// Time complexity: O(n*log(n))
+// Space complexity: O(n)
 function mergeSort(arr) {
     if (arr.length === 1) {
         return arr;
@@ -57,4 +82,40 @@ function merge(left, right) {
     return [...results, ...left, ...right];
 }
 
-module.exports = { bubbleSort, selectionSort, mergeSort, merge };
+// Time complexity: O(n^2)
+// Space complexity: O(log(n))
+function quickSort(array, left, right) {
+    const len = array.length;
+    let pivot;
+    let partitionIndex;
+
+    if (left < right) {
+        pivot = right;
+        partitionIndex = partition(array, pivot, left, right);
+
+        //sort left and right
+        quickSort(array, left, partitionIndex - 1);
+        quickSort(array, partitionIndex + 1, right);
+    }
+    return array;
+}
+
+function partition(array, pivot, left, right) {
+    let pivotValue = array[pivot];
+    let partitionIndex = left;
+
+    for (let i = left; i < right; i++) {
+        if (array[i] < pivotValue) {
+            swap(array, i, partitionIndex);
+            partitionIndex++;
+        }
+    }
+    swap(array, right, partitionIndex);
+    return partitionIndex;
+}
+
+function swap(array, firstIndex, secondIndex) {
+    var temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+}
